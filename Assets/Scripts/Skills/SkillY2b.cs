aa16963ca38506573d8fc4492bbf75e77d0ca971
xvzan <xvzan0@gmail.com>
2018-05-24 00:12:29 +0800

    SilenceSuite(Alpha)

--- Assets/Scripts/Skills/SkillY2b.cs
+++ Assets/Scripts/Skills/SkillY2b.cs
@@ -5,20 +5,60 @@ using Photon;
 
 public class SkillY2b : Photon.MonoBehaviour
 {
+    public CooldownImage MyImageScript;
+    public GameObject MySuiteObj;
+    public GameObject MySuite;
+    public float maxdistance = 10;
+    private float currentcooldown;
+    public float cooldowntime = 3;
+    public bool skillavaliable;
+    public float maxtime;
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    // Use this for initialization
+    void Start()
     {
-
+        currentcooldown = cooldowntime;
     }
 
-    void testrotate(Vector2 vec2)
+    // Update is called once per frame
+    void Update()
     {
-        vec2 = Quaternion.AngleAxis(30, Vector3.forward) * vec2;
+        if (Input.GetButtonDown("FireY") && skillavaliable)
+        {
+            DoSkill.singing = 0;
+            /*
+            if (MySuite != null)
+                MySuite.GetComponent<DestroyScript>().Destroyself();
+            MySuite = PhotonNetwork.Instantiate(MySuiteObj.name, gameObject.transform.position, Quaternion.identity, 0);
+            */
+            gameObject.GetComponent<DoSkill>().Fire = Skill;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (skillavaliable)
+            return;
+        if (currentcooldown >= cooldowntime)
+        {
+            skillavaliable = true;
+        }
+        else
+        {
+            currentcooldown += Time.fixedDeltaTime;
+            MyImageScript.IconFillAmount = currentcooldown / cooldowntime;
+        }
+    }
+
+    public void Skill(Vector2 actionplace)
+    {
+        Vector2 singplace = transform.position;
+        Vector2 skilldirection = actionplace - singplace;
+        float realdistance = Mathf.Min(skilldirection.magnitude, maxdistance) + 2;
+        if (realdistance <= 2.5)
+        {
+            return;
+        }   //半径小于自身半径时不施法
+        Vector2 Suiteplace = singplace - skilldirection.normalized * 2;
     }
 }
