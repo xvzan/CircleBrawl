9eb8def797e794e462ed4dc42997f965a44ca3bb
xvzan <xvzan0@gmail.com>
2018-06-17 18:40:11 +0800

    Add Show Nickname Function

---
+++ Assets/Scripts/ShowMyName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class ShowMyName : Photon.MonoBehaviour
+{
+    public string myname;
+
+    // Use this for initialization
+    void Start () {
+        myname = photonView.owner.NickName;
+    }
+
+    private void OnGUI()
+    {
+        Vector2 namepos = Camera.main.WorldToScreenPoint(transform.position + Vector3.down * 0.6f);
+        Vector2 nameSize = GUI.skin.label.CalcSize(new GUIContent(myname));
+        GUI.Label(new Rect(namepos.x - (nameSize.x / 2), Screen.height - namepos.y, nameSize.x, nameSize.y), myname);
+    }
+}
