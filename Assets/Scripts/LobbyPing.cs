ca4e2396c349e1510ccebe22f021ea82827b2055
xvzan <xvzan0@gmail.com>
2018-07-15 01:05:12 +0800

    Upgrade to Unity 2018.2 and Add Lobby Ping Value

---
+++ Assets/Scripts/LobbyPing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class LobbyPing : Photon.MonoBehaviour
+{
+    
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 5, 200, 30), PhotonNetwork.networkingPeer.RoundTripTime+"ms");
+    }
+}
